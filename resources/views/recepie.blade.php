fcd02ed4797f931f06a402f94758e8052a890da9
mirkojvc <mirkojovic1996@gmail.com>
2016-08-24 12:51:33 +0200

    Proba pogleda za recepte i delimicno sredjene promenljive

---
+++ resources/views/recepie.blade.php
@@ -0,0 +1,31 @@
+@include('includes.header')
+@extends('layouts.master')
+
+@section('content')
+	<div class="row">
+   			<img src=""/>
+      </div>
+      <div class="row">
+      		<div class="col-md-2"></div>
+      		<div class="col-md-6">
+      			<h1>{{ $recepie->heading }}</h1>
+      			<p>{{ $recepie->ingredients }}</p>
+      			<p>{{ $recepie->how_to_make }}</p>
+
+      		</div>
+      		<div class="col-md-4>">
+      				<div class="col-md-6>">
+      				<img src=""/>
+      				</div>
+      				<div class="col-md-6>">
+      				<img src=""/>
+      				</div>
+      				<div class="col-md-6>">
+      				<img src=""/>
+      				</div>
+      				<div class="col-md-6>">
+      				<img src=""/>
+      				</div>
+      		</div>
+      </div>
+@endsection
